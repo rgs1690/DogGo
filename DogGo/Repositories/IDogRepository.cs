ff8520f51b0cac7650e235551cf8beaca957da10
Grace Sutherland <rgs1690@gmail.com>
2022-03-19 09:41:14 -0500

    finished view models walkthrough

--- DogGo/Repositories/IDogRepository.cs
+++ DogGo/Repositories/IDogRepository.cs
@@ -12,5 +12,6 @@ namespace DogGo.Repositories
         void AddDog(Dog dog);
         void UpdateDog(Dog dog);
         void DeleteDog(int dogId);
+        List<Dog> GetDogsByOwnerId(int ownerId);
     }
 }
