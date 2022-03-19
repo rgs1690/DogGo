ffaa8dfeb3fd31b4bc345289c7533c68d2a72218
Grace Sutherland <rgs1690@gmail.com>
2022-03-18 19:53:14 -0500

    finished owner portion

--- DogGo/Repositories/IOwnerRepository.cs
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -7,6 +7,10 @@ namespace DogGo.Repositories
     public interface IOwnerRepository
     {
         List<Owner> GetAllOwners();
-        Owner GetOwnerById(int id);
+        Owner  GetOwnerById(int id);
+    
+        void  AddOwner(Owner owner);
+        void UpdateOwner(Owner owner); 
+      void DeleteOwner(int ownerId);
     }
 }
