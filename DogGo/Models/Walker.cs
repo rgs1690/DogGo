2f609a1137c68f568946ba3fc7595230787ccd81
Grace Sutherland <rgs1690@gmail.com>
2022-03-25 19:03:30 -0500

    finished validation

--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,11 +1,25 @@
-﻿namespace DogGo.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogGo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
+
+        [Required]
+        [DisplayName("Picture of Walker")]
         public string ImageUrl { get; set; }
+
+        
         public Neighborhood Neighborhood { get; set; }
     }
 }
